886b6d07d24a837caf8ee6bde96be3e0faa3fe62
cristian <crisdamencast@gmail.com>
2020-09-04 09:29:21 -0500

    Wall class created

--- Pac-Man DMC/src/game/Wall.java
+++ Pac-Man DMC/src/game/Wall.java
@@ -1,15 +1,32 @@
 package game;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Wall extends MapObject{
-	
+
 	PApplet app;
+	PImage tile;
 
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Wall (int x, int y,PApplet app) {
+		super (x,y);
+		this.app = app;
+		tile = app.loadImage("./image/tile.png");
 	}
 
+	protected void drawObject() {
+		app.image(tile,getX(),getY());
+	}
+	public boolean blocksUp(){
+	    return( this.getX()==getX() && this.getY()==getY()-18) ;
+	  }
+	public boolean blocksDown(){
+	    return( this.getX()==getX() && this.getY()==getY()-18) ;
+	  }
+	public boolean blocksLeft(){
+	    return( this.getX()==getX() && this.getY()==getY()-18) ;
+	  }
+	public boolean blocksRight(){
+	    return( this.getX()==getX() && this.getY()==getY()-18) ;
+	  }
 }
