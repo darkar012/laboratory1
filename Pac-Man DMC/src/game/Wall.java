1d7f61f7379a6bef75f319814fc1201f60b420fe
cristian <crisdamencast@gmail.com>
2020-09-04 08:53:06 -0500

    MapObject class created

---
+++ Pac-Man DMC/src/game/Wall.java
@@ -0,0 +1,15 @@
+package game;
+
+import processing.core.PApplet;
+
+public class Wall extends MapObject{
+	
+	PApplet app;
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
