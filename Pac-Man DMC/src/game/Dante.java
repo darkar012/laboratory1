74da6db8ca900c866a4d23cd092511d4c4f7f11d
cristian <crisdamencast@gmail.com>
2020-09-03 20:46:37 -0500

    Dante movement + image

--- Pac-Man DMC/src/game/Dante.java
+++ Pac-Man DMC/src/game/Dante.java
@@ -1,5 +1,56 @@
 package game;
 
-public class Dante {
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class Dante extends PApplet {
+	PApplet app;
+	PImage dante;
+	int x;
+	int y;
+	int speed;
+
+	public Dante(int x, int y, int s, PApplet app) {
+		this.x = x;
+		this.y = y;
+		this.speed = s;
+		this.app=app;
+		dante= app.loadImage("./image/dante.png");
+	}
+
+	protected void paint() {
+		app.image(dante,x,y);
+		//app.fill(255,0,0);
+		//app.rect(x, y, 50, 50);		
+	}
+
+	protected void moveLeft() {
+		x -=speed;
+		if(x <= 0){
+			x +=speed;
+		}
+	}
+	protected void moveRight() {
+
+		x +=speed;
+		if(x >= 375){
+			x -=speed;					
+		}
+
+	}
+	protected void moveUp() {
+
+		y +=speed;
+		if(y >= 0){
+			y -=speed;					
+		}
+	}
+	protected void moveDown() {
+
+		y +=speed;
+		if(y >= 667){
+			y -=speed;				
+		}
+	}
 
 }
--- Pac-Man DMC/src/game/Main.java
+++ Pac-Man DMC/src/game/Main.java
@@ -1,5 +1,81 @@
 package game;
 
-public class Main {
+	import processing.core.PApplet;
+	import processing.core.PImage;
 
-}
+	public class Main extends PApplet{
+		PImage bg;
+		public Dante player;
+		public boolean movLeft;
+		public boolean movRight;
+		public boolean movUp;
+		public boolean movDown;
+		int y;
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			PApplet.main ("game.Main");
+		}
+
+		public void settings() {
+			size(375, 667);
+		}
+		public void setup() {
+			bg = loadImage("map.png");
+			player = new Dante(92,92,5, this);
+		}
+		public void draw() {
+			background(bg);
+			player.paint();
+			if (movRight==true){
+
+				player.moveRight();
+			}
+
+			if (movLeft==true){
+
+				player.moveLeft();
+			}
+			if (movUp==true){
+
+				player.moveUp();
+			}
+
+			if (movLeft==true){
+
+				player.moveDown();
+			}
+		}
+		public void keyPressed() {
+
+			if(keyCode == RIGHT) {
+				movRight = true;
+			}
+			if(keyCode == LEFT) {
+				movLeft = true;
+			}
+			if(keyCode == DOWN) {
+				movDown = true;
+			}
+			if(keyCode == UP) {
+				movUp = true;
+			}
+
+		}
+		public void keyReleased() {
+
+			if(keyCode == RIGHT) {
+				movRight = false;
+			}
+			if(keyCode == LEFT) {
+				movLeft = false;
+			}
+			if(keyCode == DOWN) {
+				movDown = false;
+			}
+			if(keyCode == UP) {
+				movUp = false;
+			}
+	}
+	}
+
